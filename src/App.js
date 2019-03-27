95edb43ccc19a2c0cdec3e648b1fefaccc6a6a7b
avishai <avishai_taashur@gap.com>
2019-03-27 12:14:19 -0700

    adding new context

--- src/App.js
+++ src/App.js
@@ -1,13 +1,40 @@
 import React, { Component } from 'react';
 import './App.scss';
+import DataGeneral from './DataGeneral';
+import AppContextProvider from './AppContextProvider';
+
+const json = {
+  "home": {
+  "instanceName": "HomePage_031219",
+  "ciid": "16840624",
+  "type": "home",
+  "name": "HomeMultiSimple",
+  "colors":{  
+    "color":["red","blue"]}
+  }
+};
+   
+
+/*        <AppContextProvider.Consumer>
+        {(context) => (
+            <React.Fragment>
+              <div>
+             
+              </div>
+            </React.Fragment>
+          )}
+        </AppContextProvider.Consumer> */ 
 
 class App extends Component {
   render() {
+
+  
     return (
-      <div className="App">
+      <div className="App" >
         <header className="App-header">
           <h2>Welcome to SEW JSON/HTML Tool</h2>
         </header>
+        <AppContextProvider.Consumer></AppContextProvider.Consumer>
       </div>
     );
   }
