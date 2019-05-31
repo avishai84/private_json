32f64c921e4dd33e044db4845c7c1778f0d7eb04
avishai <avishai_taashur@gap.com>
2019-05-31 12:23:55 -0700

    adding wcd logo

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
 import './App.scss';
 import AppData from './AppData.js';
-
+import logo from './images/wcdLogo.png';
 class App extends Component {
 
   render() {
@@ -9,6 +9,7 @@ class App extends Component {
       <div className="App">
         <header className="App-header">
           <h2>Welcome to SEW JSON/HTML Tool</h2>
+           <img src={logo} alt="Logo" />
         </header>
         <main>
           main
@@ -16,7 +17,6 @@ class App extends Component {
         <footer>
          <AppData/>
         </footer>
-        
       </div>
     );
   }
