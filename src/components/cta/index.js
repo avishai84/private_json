72486d124ae00b82c9e4a94c818cbad8117e9451
avishai <avishai_taashur@gap.com>
2019-07-29 13:21:16 -0700

    adding CSS for dragable

---
+++ src/components/cta/index.js
@@ -0,0 +1,39 @@
+import React, { Component, Fragment} from 'react';
+import Draggable, {DraggableCore} from 'react-draggable';
+
+class DraggableComp extends Component {
+    constructor(props){
+      super(props);
+      this.state={
+        x: this.props.x,
+        link:this.prop
+      };
+    }
+
+    render() {
+        console.log(this.props.x);      
+      return ( 
+        <Draggable
+        axis="both"
+        handle=".draggingContainer"
+        defaultPosition={{x: -100, y: -100}}
+        position={null}
+        grid={[1, 1]}
+        scale={1}
+        onStart={this.handleStart}
+        onDrag={this.handleDrag}
+        onStop={this.handleStop}
+        link={this.state.link}>
+        <div>
+            <div className="draggingContainer">
+               {this.props.children}
+            </div>
+        </div>
+    </Draggable>
+   
+  
+        );
+      }
+  }
+
+  export default DraggableComp;
