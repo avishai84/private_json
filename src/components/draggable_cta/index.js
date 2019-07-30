9b5c18a941884cbd344cf707dbb85963cd669505
avishai <avishai_taashur@gap.com>
2019-07-30 11:48:13 -0700

    adding % tool tip

--- src/components/draggable_cta/index.js
+++ src/components/draggable_cta/index.js
@@ -1,5 +1,5 @@
 import React, { Component, Fragment} from 'react';
-import Draggable, {DraggableCore} from 'react-draggable';
+import Draggable, {DraggableCore, handleStop} from 'react-draggable';
 
 class DraggableComp extends Component {
     constructor(props){
@@ -7,12 +7,31 @@ class DraggableComp extends Component {
       this.state={
         x: this.props.x,
         link:this.prop,
-        desktopStyles: this.props.desktopStyles
+        desktopStyles: this.props.desktopStyles,
+        top:'',
+        left:''
       };
+      this.convertToPrecantage=this.convertToPrecantage.bind(this);
     }
 
+    handleStop(e: MouseEvent, data: Object){
+      console.log(e.x);
+      console.log(e.y);
+      console.log('Event: ', e);
+      console.log('Data: ', data);
+   
+      console.log((Number.parseFloat(e.x / 16) * 1).toFixed(2));
+      console.log((Number.parseFloat(e.y / 16) * 1).toFixed(2));
+ 
+    } 
+    convertToPrecantage(e){
+      this.setState({
+        top:(Number.parseFloat(e.y / 16) * 1).toFixed(2)+'%',
+        left: (Number.parseFloat(e.x / 16) * 1).toFixed(2)+'%'
+      });
+    }
     render() {
-    console.log(this.state.desktopStyles);
+ 
       return ( 
         <Draggable
           axis="both"
@@ -24,10 +43,18 @@ class DraggableComp extends Component {
           onStart={this.handleStart}
           onDrag={this.handleDrag}
           onStop={this.handleStop}
+          onDrag={this.convertToPrecantage}
           link={this.state.link}
-          bounds={{left:0, top: 0, right: 610, bottom: 250}}>
-          <div className="draggable draggingContainer" style={{"color":this.state.desktopStyles.color, "backgroundColor":this.state.desktopStyles.backgroundColor}}>
+          bounds={{left:-400, top: -1000, right: 1000, bottom: 1250}}>
+          <div className="draggable draggingContainer" style={{"color":this.state.desktopStyles.color, "backgroundColor":this.state.desktopStyles.backgroundColor,"fontSize":16}}>
               {this.props.children}
+              <Fragment>
+                <div className="toolTip">
+                  <span>{this.state.top}</span>
+                  <span>{this.state.left}</span>
+                </div>
+              </Fragment>
+     
           </div>
         </Draggable>
    
