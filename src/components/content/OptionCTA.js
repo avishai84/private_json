0cec3bdd31bae111068398c390f8506e7fc953a1
avishai <avishai_taashur@gap.com>
2019-08-12 10:06:32 -0700

    adding optin CTA component

---
+++ src/components/content/OptionCTA.js
@@ -0,0 +1,16 @@
+import React, {Component, Fragment} from 'react';
+
+
+class OptionCTA extends Component {
+
+redder(){
+    return(
+        <Fragment>
+            
+        </Fragment>
+    );
+};
+
+};
+
+export default OptionCTA;
